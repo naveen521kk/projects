b069aae7ab6ee68f2f08eda05ba32182615291a9
Naveen M K <naveen521kk@gmail.com>
2024-06-01 15:10:51 +0530

    Use svg use for tech-stack icons

--- src/components/react/StackIcons.tsx
+++ src/components/react/StackIcons.tsx
@@ -1,7 +1,5 @@
-import React from "react";
-
 import StackIcon from "tech-stack-icons";
 
-export function TechStackIcon({ icon }: { icon: string }) {
+export default function TechStackIcon({ icon }: { icon: string }) {
   return <StackIcon name={icon} style={{ width: "20px", height: "20px" }} />;
 }
